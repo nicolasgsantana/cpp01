4cc3d8bd1863fe3f2da0f1776ec57a2c8c6036cd
nicolasgsantana <nicolas07san@gmail.com>
2026-04-27 20:12:50 -0300

    add solution

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -1,7 +1,19 @@
 #include <string>
+#include <iostream>
+#include <memory>
 
 int	main(void)
 {
-	
+	std::string	str = "HI THIS IS BRAIN";
+	std::string	*strPtr = &str;
+	std::string	&strRef = str;
+
+	std::cout << "str    = " << &str << "\n";
+	std::cout << "strPtr = " << strPtr << "\n";
+	std::cout << "strRef = " << &strRef << "\n";
+
+	std::cout << "str    = " << str << "\n";
+	std::cout << "strPtr = " << *strPtr << "\n";
+	std::cout << "strRef = " << strRef << std::endl;
 	return (0);
 }
