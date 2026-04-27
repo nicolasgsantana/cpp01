ed93143490b7ab737d207e947edf827902ee9c1e
nicolasgsantana <nicolas07san@gmail.com>
2026-04-27 20:21:08 -0300

    add explanations

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -8,12 +8,18 @@ int	main(void)
 	std::string	*strPtr = &str;
 	std::string	&strRef = str;
 
+	std::cout << "===== MEMORY ADDRESS OF THE str =====\n";
 	std::cout << "str    = " << &str << "\n";
+	std::cout << "=== MEMORY ADDRESS HELD BY strPtr ===\n";
 	std::cout << "strPtr = " << strPtr << "\n";
-	std::cout << "strRef = " << &strRef << "\n";
+	std::cout << "=== MEMORY ADDRESS HELD BY strRef ===\n";
+	std::cout << "strRef = " << &strRef << "\n\n";
 
+	std::cout << "=========== VALUE OF str ===========\n";
 	std::cout << "str    = " << str << "\n";
+	std::cout << "==== VALUE POINTED TO BY strPtr ====\n";
 	std::cout << "strPtr = " << *strPtr << "\n";
+	std::cout << "==== VALUE POINTED TO BY strRef ====\n";
 	std::cout << "strRef = " << strRef << std::endl;
 	return (0);
 }
