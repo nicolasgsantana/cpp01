850e2a901f1a0bf82f4c9f8397e9ae570d14ac80
nicolasgsantana <nicolas07san@gmail.com>
2026-05-13 17:22:59 -0300

    add checks

--- ex04/main.cpp
+++ ex04/main.cpp
@@ -1,8 +1,28 @@
 #include <fstream>
+#include <string>
+#include <iostream>
 
 int	main(int argc, char **argv)
 {
-	(void)argc;
-	(void)argv;
+	std::string		filename;
+	std::string		s1;
+	std::string		s2;
+	std::ifstream	file;
+
+	if (argc != 4)
+	{
+		std::cerr << "Usage: ./replace filename old_word new_word\n" << std::endl;
+		return (1);
+	}
+	filename = argv[1];
+	file.open(filename.c_str(), std::ios_base::in);
+	if (!file)
+	{
+		std::cerr << "Unable to open " << filename << std::endl;
+		return (1);
+	}
+	s1 = argv[2];
+	s2 = argv[3];
+	std::cout << "Filename: " <<  filename << "\ns1: " << s1 << "\ns2: " << s2 << std::endl;
 	return(0);
 }
